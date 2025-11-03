0220f3f9c2726b434f637cef6ddd3122a128914f
BACK-0205 <back-0205@Erxess-Mac-mini.local>
2025-11-03 19:16:12 +0800

    package nemev

---
+++ index.js
@@ -0,0 +1,5 @@
+//const { startGame } = require("./print");
+
+import { startGame  } from "./print.js";
+
+startGame();
---
+++ print.js
@@ -0,0 +1,12 @@
+const print = text => {
+    console.log(text);
+};
+
+export const startGame = () => {
+    console.log("Game start");
+}
+
+// module.exports = {
+//     print,
+//     startGame
+// };
