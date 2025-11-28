d0b6e65856ba1a5fac3e2ff028d5ece9837eaaca
BACK-0205 <back-0205@Erxess-Mac-mini.local>
2025-11-28 19:12:53 +0800

    a

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
 import express from "express";
-import cors from "cors";
 import cookieParser from "cookie-parser";
 import { userRouters } from "./routers/user.js";
 import { bankRouters } from "./routers/bank.js";
 
 const app = express();
-app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/", (req, res, next) => {
   const userId = req.cookies.user;
-  console.log(userId);
-  // if (userId && req.path === "/login.html") {
-  //   return res.redirect("/bank.html");
-  // }
 
-  // if (!userId && req.path === "/bank.html") {
-  //   return res.redirect("/login.html");
-  // }
+  if (userId && req.path === "/login.html") {
+    return res.redirect("/bank.html");
+  }
+
+  if (!userId && req.path === "/bank.html") {
+    return res.redirect("/login.html");
+  }
 
   if (userId) {
     const user = {
